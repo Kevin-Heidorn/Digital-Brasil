234278c6162070ba7056618402aae5296f75dc76
Kevin-Heidorn <kevinheidorn20@gmail.com>
2024-09-13 15:52:29 -0300

    Soluções criado

---
+++ scripts/soluções.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
+  
+    hamburger.addEventListener('click', function() {
+      navLinks.classList.toggle('nav-active');
+    });
+  });
+  
