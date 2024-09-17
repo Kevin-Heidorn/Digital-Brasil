6f257c61b97914c03bea36c69cec33bc229d2c00
Kevin-Heidorn <kevinheidorn20@gmail.com>
2024-09-17 18:36:06 -0300

    mudança e teste

--- scripts/projetos.js
+++ scripts/projetos.js
@@ -98,3 +98,15 @@ document.addEventListener('DOMContentLoaded', () => {
   });
   
 
+  
+document.addEventListener('DOMContentLoaded', function() {
+  const hamburger = document.querySelector('.hamburger');
+  const navLinks = document.querySelector('.nav-links');
+
+  hamburger.addEventListener('click', function() {
+    // Alterna a classe 'nav-active' para mostrar ou esconder o menu lateral
+    navLinks.classList.toggle('nav-active');
+  });
+});
+
+
